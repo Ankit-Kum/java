1fa76bf0a3455a4a1e12f50f4ba23d902c2bbbe3
prathamtalele <134584728+prathamtalele@users.noreply.github.com>
2023-05-27 09:49:13 +0530

    Update First.java
    changes add

--- Demo/src/d/First.java
+++ Demo/src/d/First.java
@@ -4,7 +4,7 @@ public class First {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+                   int num = 10;
 		 int myNum = 15;
    		 System.out.println(myNum);
 		System.out.println(mby New );
