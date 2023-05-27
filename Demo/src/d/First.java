5b7d223e1f5aa60485946c88c56323597c0ed579
ANkit-DEV <your_email@example.com>
2023-05-27 00:11:00 -0300

    modifified the code by tester brnach

--- Demo/src/d/First.java
+++ Demo/src/d/First.java
@@ -7,7 +7,7 @@ public class First {
 
 		    int sum1 = 100 + 50;
 		    int sum2 = sum1 + 250;
-		    int sum3 = sum2 + sum2;
+		    int sum3 = sum2 - sum2;
 		    System.out.println(sum1);
 		    System.out.println(sum2);
 		    System.out.println(sum3); 
