778b1810265d2950092811ec2e170d1057e381a2
pramodsutar2608 <134577702+pramodsutar2608@users.noreply.github.com>
2023-05-27 09:36:45 +0530

    Update First.java
    Done changes please check

--- Demo/src/d/First.java
+++ Demo/src/d/First.java
@@ -3,11 +3,19 @@ package d;
 public class First {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-               int num = 5;
-		 int myNum = 15;
-   		 System.out.println(myNum);
-		System.out.println(mby New );
+		double number = 12.3;
+
+        // true if number is less than 0
+        if (number < 0.0)
+            System.out.println(number + " is a negative number.");
+
+        // true if number is greater than 0
+        else if ( number > 0.0)
+            System.out.println(number + " is a positive number.");
+
+        // if both test expression is evaluated to false
+        else
+            System.out.println(number + " is 0.");
 	}
 
 }
